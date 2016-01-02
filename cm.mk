d709ffcd1b5e85811a6f42cfb91ef80fa36d5fa0
corphish <d97.avinaba@gmail.com>
2016-01-02 13:54:13 +0530

    Update build desc and fingerprint

--- cm.mk
+++ cm.mk
@@ -13,8 +13,8 @@ $(call inherit-product, device/samsung/mint2g/full_mint2g.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mint \
     TARGET_DEVICE=mint \
-    BUILD_FINGERPRINT="samsung/mintxx/mint:4.1.2/JZO54K/S5282XXAMEA:user/release-keys" \
-    PRIVATE_BUILD_DESC="mintxx-user 4.1.2 JZO54K S5282XXAMEA release-keys"
+    BUILD_FINGERPRINT="samsung/mintxx/mint:4.1.2/JZO54K/S5282XXANF3:user/release-keys" \
+    PRIVATE_BUILD_DESC="mintxx-user 4.1.2 JZO54K S5282XXANF3 release-keys"
     
 
 PRODUCT_NAME := cm_mint2g
