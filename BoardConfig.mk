c01d73ac72e4dba38209f5e1beedc3a73829ab94
Avinaba Dalal <d97.avinaba@gmail.com>
2016-02-08 16:09:32 +0530

    BoardConfig: Set CPU variant to cortex-a9
    Generic cpu causes issues with ART, so switch to cortex-a9 as its working in here.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,8 @@ TARGET_OTA_ASSERT_DEVICE := mint,mint2g,GT-S5282,GT-S5280
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a
-TARGET_ARCH_VARIANT_CPU := generic
-TARGET_CPU_VARIANT := generic
+TARGET_ARCH_VARIANT_CPU := cortex-a9 #original : coretx-a5
+TARGET_CPU_VARIANT := cortex-a9 #original : coretx-a5
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 ARCH_ARM_HAVE_TLS_REGISTER := true
