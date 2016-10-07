aedad115d6c50ec95a9f89f615a41df2d6fb1bbb
lehkeda <computer.boy71@yahoo.com>
2016-10-07 16:44:25 +0200

    Revert "mint2g: Use default gcc 4.9"
    This reverts commit a530e34f12ec5c54171a970c485b1500b0a39ee6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/mint2g
 TARGET_KERNEL_CONFIG := cyanogenmod_mint_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
 
 
 # Partitions
