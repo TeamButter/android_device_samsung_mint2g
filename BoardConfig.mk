e576e6c516d14451f4c40062c476fa4d73038de0
Avinaba Dalal <d97.avinaba@gmail.com>
2017-08-25 07:18:58 +0000

    mint2g: Enable offmode charging
    - Still needs to fix tho.
    - Earlier, without this commit, device would boot straight away into recovery.
    - With this commit, offmode charging starts but does not work.
    - That is we can see the battery, but no animations (and probably no charging?).

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,7 +143,10 @@ TARGET_SCREEN_WIDTH := 240
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
+BOARD_NO_CHARGER_LED := true
 
 # Healthd
 WITH_CM_CHARGER := false
