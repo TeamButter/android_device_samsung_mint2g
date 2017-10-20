cc9f7aabdd90a2f86639b217a0b9326ff50bf7e6
lehkeda <compute.boy71@yahoo.com>
2017-10-20 17:20:20 +0545

    Low memory optimization

--- device_mint2g.mk
+++ device_mint2g.mk
@@ -178,6 +178,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.image-dex2oat-Xmx=48m \
 	dalvik.vm.image-dex2oat-filter=speed
 
+# Low ram optimization
+PRODUCT_PROPERTY_OVERRIDES += ro.config.low_ram=true
+
 
 # Force use old camera api
 PRODUCT_PROPERTY_OVERRIDES += \
