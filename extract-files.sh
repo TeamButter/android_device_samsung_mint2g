a64b051f8069b022a82ce46bf90f9cac3e0af433
Cswl Coldwind <cswl1337@gmail.com>
2017-10-23 22:29:09 +0545

    extract-files.sh: fix adb pull and bring back copying from system dump also remove vendor overlays
    you can now use "device" to pull from device
    or give path to system dump

--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,7 @@
 
 # Copyright (C) 2012-2016, The CyanogenMod Project
 # Copyright (C) 2017, The LineageOS Project
+# Copyright (C) 2017, Psych Half <psych.half@gmail.com>
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,17 +20,38 @@ DEVICE=mint2g
 DEVICEOUTDIR=vendor/$VENDOR/$DEVICE
 DEVICEBASE=../../../$DEVICEOUTDIR/proprietary
 DEVICEMAKEFILE=../../../$DEVICEOUTDIR/$DEVICE-vendor-blobs.mk
-adb root
-adb wait-for-device
 
-echo "Pulling device specific files..."
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+
+# Clean up old contents
+rm -rf DEVICEBASE/*
+
+if [ -z "$1" ]; then
+  echo "Nothing specified."
+  echo "Use device to pull from device or path to system dump"
+  exit -1
+elif [ $1 == "device" ]; then 
+  adb root
+  adb wait-for-device
+
+  echo "Pulling device specific files..."
+  for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+      DIR=`dirname $FILE`
+      if [ ! -d $DEVICEBASE/$DIR ]; then
+          mkdir -p $DEVICEBASE/$DIR
+      fi
+      adb pull "/system/$FILE" $DEVICEBASE/$FILE
+  done
+else
+# we got path to system dump
+  for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
     DIR=`dirname $FILE`
     if [ ! -d $DEVICEBASE/$DIR ]; then
-        mkdir -p $DEVICEBASE/$DIR
+      mkdir -p $DEVICEBASE/$DIR 
     fi
-    adb pull /$FILE $DEVICEBASE/$FILE
+    # My way of pulling blobs without the device
+    cp $1/$FILE $DEVICEBASE/$FILE
 done
+fi
 
 
 (cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__VENDOR__/$VENDOR/g > $DEVICEMAKEFILE
@@ -99,7 +121,7 @@ EOF
 # limitations under the License.
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS += vendor/__VENDOR__/__DEVICE__/overlay
+#DEVICE_PACKAGE_OVERLAYS += vendor/__VENDOR__/__DEVICE__/overlay
 
 \$(call inherit-product, vendor/__VENDOR__/__DEVICE__/__DEVICE__-vendor-blobs.mk)
 EOF
